fb745de18da8011a46b631fb48b42fd0c349938b
Red <red@IronClad.defense>
2021-10-18 08:05:44 -0400

    updated rewards with score and time tracking, page and tile check, and enemy distance functions

--- GhostNet/gn.py
+++ GhostNet/gn.py
@@ -1,19 +1,25 @@
 import sys
 import nintaco
-from random import seed
+import random
 from random import randint
+import math
+
 
 #Use localhost if game is played on the same machine or use IP to use another machine on the network
 
 nintaco.initRemoteAPI("192.168.1.182", 9999)
 api = nintaco.getAPI()
 
-STRING = "FITNESS: 00000"
-WALKED = "FITNESS: "
-STRING_1 = "Gen: 1 Mutation: A"
+STRING = "REWARDS: 00000"
+WALKED = "REWARDS: "
+PAGE = "PAGE: "
+TILE = " TILE: "
+STRING_1 = PAGE + TILE
+
 global maxFit
+global rewardi
 WALKED_VAL = 0
-maxFit = 0
+maxScore = 0
 
 SPRITE_ID = 123
 SPRITE_SIZE = 32
@@ -37,6 +43,9 @@ def apiEnabled():
   global strY
   global strX1
   global strY1
+
+  api.setSpeed(0) #maximum emulation speed
+  print("They've gone plaid!")
 
   print("API enabled")
   strWidth = api.getStringWidth(STRING, False)
@@ -116,6 +125,51 @@ def getY():
     
     return  y
 
+def getTile():
+    x = api.getCameraX()
+    y = getY()-16
+   
+    subx = math.floor(x/8)
+    suby = math.floor((y-32)/16)
+    #print("tile:", subx)
+    return int(subx)
+
+def getPage():
+    x = getWalked()
+    pageNo = math.floor(x/512)
+    #print("Page:", pageNo)
+    return int(pageNo)
+
+def getScore():
+    sc1 = api.readCPU(0x06E8) #100s
+    sc2 = api.readCPU(0x06E6) #1000s
+    sc3 = api.readCPU(0x06E4) #10000s
+        
+    if sc1 ==255:  #Score bytes initialize to 255 and need to be zeroed before display
+        sc1 = 0
+    if sc2 == 255:
+        sc2 = 0
+    if sc3 == 255:
+        sc3 = 0
+        
+    score = (sc1*100)+(sc2*1000)+(sc3*10000)
+    #print(score) 
+    return score
+def levelComplete():
+    completed = false 
+    if api.readCPU(0x0178) ==7:
+        completed = true
+    return completed
+
+def getTime():
+    t1 = api.readCPU(0x06FC) #1S
+    t2 = api.readCPU(0x06FA) #10s
+    t3 = api.readCPU(0x06F6) #Minutes
+
+    time = t1 + (t2*10) + (t3*60) #time in seconds
+
+    return time
+
 def detectEnemy():
     enemies = {}
     for slot in range(0,7):
@@ -124,86 +178,90 @@ def detectEnemy():
 
             ex = api.readCPU(515 + slot*4*6)
             ey = api.readCPU(0x200 + slot*4*6)
-            enemies[len(enemies)] = {"x": ex, "y": ey}
-            #if ex != 255 and  ey != 255 and  abs(ey-getY())<48 and  abs(ex-getX())>16 and (ex <120 or ex >130):
+         #   enemies[len(enemies)] = {"x": ex, "y": (ey)}
+            if ex != 255 and  ey != 255 and  abs(ey-getY())<48 and  abs(ex-getX())>16 and (ex <120 or ex >130):
+                #ex = api.readCPU(515 + slot*4*6)
+                #ey = api.readCPU*0x200 + slot*4*6+12)
+                enemies[len(enemies)]={"x": ex, "y": ey}
             
                # print(abs(ey-getY()))
-             #   api.drawRect(ex,ey-12, 16,32)
-              #  api.drawLine(126, getY(),ex,ey-12)
+                api.drawRect(ex,ey-12, 16,32)
+                api.drawLine(126, getY(),ex,ey+12)
 
     return enemies
 
-def getInputs():
+def enemyDistance():
     enemies = detectEnemy()
-    inputs = []
-    for dy in range(-BoxRadius*16, BoxRadius*16 +1, 16):
-        for dx in range(-BoxRadius*16, BoxRadius*16+1, 16):
-            inputs[len(inputs)] = 0
-            
-            for i in range(0, len(enemies)):
-                distx = abs(enemies[i]["x"] - (getX()+dx))
-                disty = abs(enemies[i]["y"] - (getY()+dy))
+    enemyDist = {}
+    for i in range(0, len(enemies)):
+        distx = abs(enemies[i]["x"] - getX())
+        disty = abs(enemies[i]["y"] - getY())
+        enemyDist[len(enemyDist)]={"x": distx, "y": disty}
 
-                if distx <=8 and disty <=8:
-                    inputs[len(inputs)] = -1
-    return inputs
+        if disty < 32:
+            api.drawLine(126, getY(), enemies[i]["x"], enemies[i]["y"])
+            api.drawRect(enemies[i]["x"], enemies[i]["y"]-12, 16, 32)
 
-    #ZOMBIE DETECTOR
-    #if api.readCPU(0x025E)!=0:
-        #print("Zombie Detected!")
-     #   api.drawRect(api.readCPU(0x0203),api.readCPU(0x0220)-12, 16, 32)
-    #CROW DETECT
-    #if api.readCPU(0x022E)!=0:
-    #    api.drawRect(api.readCPU(0x006A), api.readCPU(0x0238), 16, 32)
-    #SATAN DETECT
-    #if api.readCPU(0x022E)!=0:
-    #    api.drawRect(api.readCPU(0x0233+8), api.readCPU(0x0220),16, 24)
+    return enemyDist
 
-    #FIRST BOSS DETECT
-    #if api.readCPU(0x023A)!=0:
-    #    api.drawRect(api.readCPU(0x020F),api.readCPU(0x03FC),32,40)
+def getReward():
+    walked = getWalked()/3 #ultimate goal is to move right, so assign more weight to this goal
+    score = getScore()/10  
+    time = getTime()
+    timeScore = 10*(120 - time) #lose 10 points of reward for every second lost.
 
+    reward = score+walked-timeScore
+
+    return reward
+  
     
 def randomCommand():
-    value = randint(0, 7)
-    if value == 0:
+    buttonList = ['A'] * 50 + ['B'] * 50 + ['Up'] * 10 + ['Down'] * 10 + ['Left'] * 5 + ['Right'] * 90
+    #value = 5
+    value =  random.choice(buttonList)
+    #print(value)
+    #value =  random.choices(buttonList, weights=(20, 20, 10, 10, 5, 55), k=1)
+    #value = randint(0, 5)
+    if value == 'A':
         pressA()
-    if value == 1:
+    if value == 'B':
         pressB()
-    if value == 2:
-        print("select pressed")
-    if value == 3:
-        print("start pressed")
-    if value == 4:
+    if value == 'Up':
         moveUp()
-    if value == 5:
+    if value == 'Down':
         moveDown()
-    if value == 6:
+    if value == 'Left':
         moveLeft()
-    if value == 7:
+    if value == 'Right':
         moveRight()
 
 
 
 def renderFinished():
-    global maxFit
-    tempX = getWalked()
-    WALKED_VAL =tempX/3
-    #calcDist()
-    detectEnemy()
-    if WALKED_VAL > maxFit:
-        maxFit = WALKED_VAL
-       # print("maxFit is ", maxFit)
+    global maxScore    
+    reward = getReward()
+    if reward > maxScore:
+        maxScore = reward
+        print("Maximum is", maxScore)
 
-    STRING = WALKED + str(WALKED_VAL)
+    if levelComplete() ==true:
+        maxScore+=99999999
+        print("Maximum is", maxScore)
+        #api.writeGamepad(0, Start, true)
+
+    #detectEnemy()
+    enemyDistance()
+    
+    STRING_1 = (PAGE + str(getPage()))+ (TILE + str(getTile())) 
+    STRING = WALKED + str(reward)
     api.setColor(nintaco.DARK_BLUE)
     api.fillRect(strX - 1, strY - 1, strWidth + 2, 9)
     api.setColor(nintaco.BLUE)
     api.drawRect(strX - 2, strY - 2, strWidth + 3, 10)
     api.setColor(nintaco.DARK_BLUE)
-    api.fillRect(strX1 - 1, strY1 - 1, strWidth1 + 2, 9)
+    api.fillRect(strX1 - 1, strY1 - 1, strWidth1 + 24, 9)
     api.setColor(nintaco.BLUE)
-    api.drawRect(strX1 - 2, strY1 - 2, strWidth1 + 3, 10)
+    api.drawRect(strX1 - 2, strY1 - 2, strWidth1 + 25, 10)
     api.setColor(nintaco.WHITE)
     api.drawString(STRING, strX, strY, False)
     api.drawString(STRING_1, strX1, strY1, False)
@@ -213,16 +271,15 @@ def renderFinished():
     
 #ENTER RANDOM COMMAND FOR TESTING
     
-    #randomCommand()
+    randomCommand()
     #moveRight()
     if api.readCPU(0x0715)==2: #START WITH 3 LIVES, IF WE DIE, RESTART AT BEGINNING STATE.
+        reward-=500 #placeholder punishment value
         api.setSpeed(1)
         
-        api.reset()
-    
+        #api.reset()
         api.loadState("states/G&G.save")
-        #api.quickLoadState(1)
-        #api.setSpeed(100)     
+        #api.setSpeed(0) #Set emulation levels to maximum speed. 
         WALKED_VAL = 0    #RESET FITNESS TO 0 due to death.
         #apiEnabled()
         #launch()
